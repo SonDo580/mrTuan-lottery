29e85473b5aeab2151bf8aae1d66bb1033d9f990
Son Do <dominhson580@gmail.com>
2022-09-19 21:12:15 +0700

    Create new file: banner.js

---
+++ banner.js
@@ -0,0 +1,10 @@
+const bannerImages = document.querySelectorAll("section.banner img");
+setInterval(() => {
+  for (let image of bannerImages) {
+    if (image.classList.contains("active")) {
+      image.classList.remove("active");
+    } else {
+      image.classList.add("active");
+    }
+  }
+}, 3000);
--- main.js
+++ main.js
@@ -12,17 +12,6 @@ $("section.slider .owl-carousel").owlCarousel({
   },
 });
 
-const bannerImages = document.querySelectorAll("section.banner img");
-setInterval(() => {
-  for (let image of bannerImages) {
-    if (image.classList.contains("active")) {
-      image.classList.remove("active");
-    } else {
-      image.classList.add("active");
-    }
-  }
-}, 3000);
-
 const winnerCards = document.querySelectorAll("section.result .card-winner");
 const numWinners = winnerCards.length;
 setInterval(() => {
