008eff91178d15f1e2b0525ddf11f89e137456df
Son Do <dominhson580@gmail.com>
2022-09-29 10:06:22 +0700

    Close mobile-menu when click close-menu button

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const toggleMenuButton = document.querySelector(".toggle-menu");
+const showMenuButton = document.querySelector(".show-menu");
+const closeMenuButton = document.querySelector(".close-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-toggleMenuButton.addEventListener("click", showMenu);
+showMenuButton.addEventListener("click", showMenu);
+closeMenuButton.addEventListener("click", closeMenu);
 
 function showMenu() {
   mobileMenu.classList.add("show");
+}
+
+function closeMenu() {
+  mobileMenu.classList.remove("show");
 }
